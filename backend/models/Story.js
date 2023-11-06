9f7daffcaf34774a766431987e41a332e82c1512
glendonchin <glendonchin@gmail.com>
2023-11-06 12:56:39 -0500

    Implement category based filtering

--- backend/models/Story.js
+++ backend/models/Story.js
@@ -4,6 +4,8 @@ const storySchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  // Adding category as a reference to a Category model
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
 });
 
 module.exports = mongoose.model('Story', storySchema);
--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllStories, createStory } = require('../controllers/storiesController');
+const {
+  getAllStories,
+  createStory,
+  getStoriesByCategory
+} = require('../controllers/storiesController');
 
 router.get('/', getAllStories); // Get all stories
 router.post('/', createStory); // Create a new story
+router.get('/category/:categoryId', getStoriesByCategory); // Get stories by category ID
 
 module.exports = router;
