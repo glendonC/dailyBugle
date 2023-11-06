392b6ab773b8fd921847a0204d7fe79ce5fdd37b
glendonchin <glendonchin@gmail.com>
2023-11-06 16:02:25 -0500

    Header and Footer components

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      {/* Footer content goes here */}
+      <p>&copy; {new Date().getFullYear()} The Daily Bugle</p>
+    </footer>
+  );
+}
+
+export default Footer;
