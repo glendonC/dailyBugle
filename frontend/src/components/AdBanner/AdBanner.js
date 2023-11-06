042d5d7c9def23452bd03213e657480181c567ae
glendonchin <glendonchin@gmail.com>
2023-11-06 16:02:03 -0500

    Ad Banner component

---
+++ frontend/src/components/AdBanner/AdBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdBanner() {
+  // Placeholder for ad content
+  return (
+    <div className="ad-banner">
+      {/* Ad content or image goes here */}
+      <p>Advertisement</p>
+    </div>
+  );
+}
+
+export default AdBanner;
