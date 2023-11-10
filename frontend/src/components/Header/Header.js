b8e004f1b3d6e7b4554ea9dff913a65f217e8ba9
glendonchin <glendonchin@gmail.com>
2023-11-10 09:53:21 -0500

    Add sign out button

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'; // If you are using react-router
 
-// Assuming you have a functional component
 const Header = () => {
-  return <div>Your Header Markup Here</div>;
+  const history = useHistory();
+
+  const handleSignout = () => {
+    // Clear authentication tokens or session data here
+    localStorage.removeItem('token'); // Example if you're using localStorage
+
+    // Redirect to login page or home page
+    history.push('/login');
+  };
+
+  return (
+    <div>
+      Your Header Markup Here
+      <button onClick={handleSignout}>Sign Out</button>
+    </div>
+  );
 };
 
 export default Header;
