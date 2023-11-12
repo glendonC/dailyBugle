a2e62d543ba7498dfbea6db1606b2c8dcb757e72
glendonchin <glendonchin@gmail.com>
2023-11-12 14:14:27 -0500

    Track ad clicks and impressions

--- backend/routes/adRoutes.js
+++ backend/routes/adRoutes.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const router = express.Router();
-const { trackInteraction } = require('../controllers/advertisementsController');
+const AdInteraction = require('../models/AdInteraction');
+const Advertisement = require('../models/Advertisement');
+
+// Track ad impressions
+router.post('/impression', async (req, res) => {
+    const { adId, userId } = req.body; // Extract adId and userId from request body
+
+    try {
+        const newInteraction = new AdInteraction({
+            ad: adId,
+            user: userId, // Use userId from request
+            type: 'impression'
+        });
+        await newInteraction.save();
+        res.status(200).json({ message: "Impression recorded" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error recording impression" });
+    }
+});
 
 // Track ad clicks
-router.post('/ad/:adId/click', trackInteraction);
+router.post('/click', async (req, res) => {
+    const { adId, userId } = req.body; // Extract adId and userId from request body
+
+    try {
+        const newInteraction = new AdInteraction({
+            ad: adId,
+            user: userId, // Use userId from request
+            type: 'click'
+        });
+        await newInteraction.save();
+        res.status(200).json({ message: "Click recorded" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error recording click" });
+    }
+});
+// Get a random ad
+router.get('/random', async (req, res) => {
+    try {
+        const count = await Advertisement.countDocuments();
+        const random = Math.floor(Math.random() * count);
+        const ad = await Advertisement.findOne().skip(random);
+        res.json(ad);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching random ad" });
+    }
+});
 
 module.exports = router;
--- frontend/src/components/AdBanner/AdBanner.js
+++ frontend/src/components/AdBanner/AdBanner.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import { Box, Typography, Paper } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Paper, Modal, Button } from '@mui/material';
+import { useAuth } from '../../AuthContext';
 
 function AdBanner() {
-  return (
-    <Box my={2} p={2} component={Paper} elevation={4}>
-      <Typography variant="subtitle1" align="center" color="textSecondary">
-        Advertisement
-      </Typography>
-      {/* Add additional ad content or image here */}
-    </Box>
-  );
+    const [ad, setAd] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
+    const { auth } = useAuth(); // Use the useAuth hook to access the auth state
+
+    useEffect(() => {
+        fetch('http://localhost:5001/api/ads/random')
+            .then(response => response.json())
+            .then(data => {
+                setAd(data);
+                fetch('http://localhost:5001/api/ads/impression', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ adId: data._id, userId: auth.userId })
+                });
+            })
+            .catch(error => console.error('Error fetching random ad:', error));
+    }, [auth.userId]); // Add auth.userId as a dependency
+
+    const handleAdClick = () => {
+        if (ad) {
+            fetch('http://localhost:5001/api/ads/click', { // Changed to /click
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ adId: ad._id, userId: auth.userId })
+            });
+            setOpenModal(true); // Open the modal on ad click
+        }
+    };
+
+    const handleCloseModal = (event) => {
+        event.stopPropagation();
+        setOpenModal(false); // Close the modal
+    };
+
+    if (!ad) return null;
+
+    return (
+        <Box my={2} p={2} component={Paper} elevation={4} onClick={handleAdClick}>
+            <Typography variant="subtitle1" align="center" color="textSecondary">
+                {ad.title} {/* Display ad title */}
+            </Typography>
+            {/* Add additional ad content or image here */}
+
+            <Modal open={openModal} onClose={handleCloseModal}>
+                <Box style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: 20 }}>
+                    <Typography variant="h6">{ad.title}</Typography>
+                    <Typography variant="body1">{ad.content}</Typography>
+                    {/* Display more ad details or content here */}
+                    <Button onClick={handleCloseModal}>Close</Button>
+                </Box>
+            </Modal>
+        </Box>
+    );
 }
 
 export default AdBanner;
