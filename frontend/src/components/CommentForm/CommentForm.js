5db8d33bb1804946f9c29bc9d4cab7933c9e58d2
glendonchin <glendonchin@gmail.com>
2023-11-12 11:43:46 -0500

    Update CommentForm styling

--- frontend/src/components/CommentForm/CommentForm.js
+++ frontend/src/components/CommentForm/CommentForm.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
+import { TextField, Button, Box } from '@mui/material';
 
 const CommentForm = ({ onCommentSubmit }) => {
-  const [content, setContent] = useState('');
+    const [content, setContent] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onCommentSubmit(content);
-    setContent('');
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onCommentSubmit(content);
+        setContent('');
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Write a comment..."
-        required
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <Box mb={2}>
+                <TextField
+                    fullWidth
+                    multiline
+                    rows={3}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="Write a comment..."
+                    variant="outlined"
+                    margin="normal"
+                    required
+                />
+                <Button type="submit" variant="contained" color="primary">Submit</Button>
+            </Box>
+        </form>
+    );
 };
 
 export default CommentForm;
