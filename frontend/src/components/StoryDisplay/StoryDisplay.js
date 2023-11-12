da4f9800a913af9e09c8bac0e986f2c252a004f2
glendonchin <glendonchin@gmail.com>
2023-11-12 16:46:11 -0500

    Re insert Story Display

---
+++ frontend/src/components/StoryDisplay/StoryDisplay.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import CommentList from '../CommentList/CommentList';
+import CommentForm from '../CommentForm/CommentForm';
+import AdBanner from '../AdBanner/AdBanner';
+
+const StoryDisplay = () => {
+  const [story, setStory] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchStory = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/stories/${id}`);
+        const data = await response.json();
+        setStory(data);
+      } catch (error) {
+        console.error('Error fetching story:', error);
+      }
+    };
+
+    fetchStory();
+  }, [id]);
+
+  if (!story) {
+    return <div>Loading story...</div>;
+  }
+
+  const handleCommentSubmit = (commentContent) => {
+    console.log('Comment submitted:', commentContent);
+  };
+
+  return (
+    <div className="story-row">
+      <div className="story-content">
+        <div className="story">
+          <h2>{story.title}</h2>
+          <p>{story.content}</p>
+        </div>
+        <div className="comment-section">
+          <CommentForm onCommentSubmit={handleCommentSubmit} />
+          <CommentList comments={story.comments} />
+        </div>
+      </div>
+      <div className="ad-section">
+        <AdBanner />
+      </div>
+    </div>
+  );
+};
+
+export default StoryDisplay;
